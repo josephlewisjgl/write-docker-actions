7c86e2fbbd26acae6efdb6c029e220cdab6e2452
Joe Lewis <josephgeorgelewis2000@gmail.com>
2021-08-07 11:47:56 +0100

    Update main.py - new API
    Use a different API as cat-facts cannot be accessed

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -2,18 +2,14 @@ import requests
 import sys
 import random
 
-# Make an HTTP GET request to the cat-fact API
-cat_url = "https://cat-fact.herokuapp.com/facts"
+cat_url = "https://catfact.ninja/facts?limit=50"
 r = requests.get(cat_url)
-r_obj_list = r.json()
+r_obj_list = r.json()["data"]
 
-# Create an empty list to store individual facts in
-# This will make it easy to select a random one later
 fact_list = []
 
-# Add the "text" of every object into the fact_list list
 for fact in r_obj_list:
-    fact_list.append(fact["text"])
+    fact_list.append(fact["fact"])
 
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
